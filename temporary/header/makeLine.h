0d2a1cf6eaa6e7257e79573f096c62b768e0a917
Nachanjin <skxkswls@gmail.com>
2022-11-26 17:33:37 +0900

    pull

---
+++ temporary/header/makeLine.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "dataStruct.h"
+#include <vector>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include <stack>
+
+void tokenized(string str, int& line, string& name, W& weight, int& flag);
+Station* allocStation(string str, W& weight, int& flag);
+Edge* allocEdge(int line, W weight, Station* station);
+void swapStaion(Station*& n, Station*& current, Station*& N, W& nWeight, W& weight, W& Nweight, int& flag, int& Nflag);
+void middleStation(Station* n, Station* current, Station* N, W nWeight, W weight);
+void lastStation(Station* n, Station* current, W nWeight);
+void makeLine(Station*& s, vector<string> str);
